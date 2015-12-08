61cf5ac91ff42ae6cdf1067dd91a3c0a94a4ef25
amandareilly93 <amandareilly93@gmail.com>
2015-12-08 15:48:00 -0500

    update with type of new infection

--- assumptions.R
+++ assumptions.R
@@ -1,2 +1,5 @@
-# 1) at least one infection is present every year; if none are present, we artificially introduce one each year 
-# 2) immunity wanes continuously, not in discrete chunks, but we model certain levels of immunity
+# 1) either a) or b), we need to decide
+#   a) at least one infection is present every year; if none are present, we artificially introduce one at the beginning of each year 
+#   b) low level of constant spontaneous new infection (from S_0 to I_0)
+# 2) immunity wanes continuously, not in discrete chunks, but we model certain levels of immunity ()
+# 3) 
